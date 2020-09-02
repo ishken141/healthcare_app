65b46730e19d4b8b4ca43f3392c839dbe6915b44
ishken141 <ishikenken141@icloud.com>
2020-09-02 20:06:54 +0900

    update

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,8 @@ class RoomsController < ApplicationController
   def index
     @users = User.all 
   end 
+  def destroy 
+  end 
   def show  
     @user = User.find(params[:id]) 
     @message = Message.new 
